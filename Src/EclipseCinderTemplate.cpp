cbe70ba605ac7bc98c42ab4841c31cb28a0a4075
guojian miguel wu <guojenman@gmail.com>
2011-07-19 09:40:15 -0400

    cleanup

--- Src/EclipseCinderTemplate.cpp
+++ Src/EclipseCinderTemplate.cpp
@@ -8,9 +8,8 @@
 
 class EclipseCinderTemplate : public ci::app::AppBasic {
 public:
-	void setup();
 	void prepareSettings( ci::app::AppBasic::Settings *settings );
-	void mouseDown( ci::app::MouseEvent event );
+	void setup();
 	void update();
 	void draw();
 };
@@ -22,32 +21,18 @@ void EclipseCinderTemplate::prepareSettings( ci::app::AppBasic::Settings *settin
 
 void EclipseCinderTemplate::setup()
 {
-	std::cout << "Setting application path: " << getAppPath() << std::endl;
-	chdir( getAppPath().c_str( ) );
 
-	//[[NSApplication sharedApplication] activateIgnoringOtherApps:YES]
 }
 
-void EclipseCinderTemplate::mouseDown( ci::app::MouseEvent event )
-{
-}
 
 void EclipseCinderTemplate::update()
 {
+
 }
 
 void EclipseCinderTemplate::draw()
 {
-	// clear out the window with black
-	ci::Color aColor = ci::Color( 0, 0, 0 );
-	aColor.r = fabs( cosf(getElapsedFrames() * 0.008) );
-	aColor.g = fabs( sinf(getElapsedFrames() * 0.01) );
-	aColor.b = (float) getMousePos().x / getWindowWidth();
 
-	ci::gl::clear( aColor );
-
-	ci::gl::color( ci::Color(aColor * 0.5) );
-	ci::gl::drawLine( ci::Vec2f(getMousePos()), ci::Vec2f( getWindowCenter() ) );
 }
 
 CINDER_APP_BASIC( EclipseCinderTemplate, ci::app::RendererGl )
